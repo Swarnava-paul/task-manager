0e87ddc76f7eddf3375655c049f5244dd96b9b7d
Swarnava-paul <swarnavapaul108@gmail.com>
2024-09-10 17:08:17 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,41 @@ import './App.css'
 import React from 'react'
 import { Suspense } from 'react'
 const Task = React.lazy(()=>import('./components/Task'));
+import { useRef } from 'react'
 
 function App() {
   const [task,setTask] = useState([])
- 
+  const inputRef = useRef(null)
+  const [priority,setPriority] = useState()
+console.log(task);
+
   return (
     <>
     <div style={{paddingBottom:"30px"}}>
       Add Task
-      <input type="text" />
-      <select>
+      <input ref={inputRef} type="text" />
+
+      <select onChange={(e)=>{
+        setPriority(e.target.value)
+        
+      }}>
         <option value="">Select Priority</option>
         <option value="high">high</option>
         <option value="low">low</option>
       </select>
-      <button>Add task</button>
+      <button onClick={()=>{
+       const array = [...task];
+
+       array.unshift({name:[inputRef.current.value],priority:priority})
+       setTask(array)
+      }}>Add task</button>
     </div>
     <Suspense fallback={<>loading component...</>}>
      {
       task && task.map((i,index)=> (
-        <Task key={index}/>
+        <Task key={index} task={task}
+         setTask={setTask} index={index} 
+         props={i}/>
       ))
      }
     </Suspense>
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,8 +1,42 @@
+import { useState } from "react";
+const Task = ({props,index,task,setTask}) => {
+    const [edit,setEdit] = useState(false)
+    const {name,priority} = props;
+    const [taskValue,setTaskValue] = useState(name)
 
-const Task = ({props}) => {
+    const handleDelete = () => {
+        const taskArray = [...task];
+        const remainings = taskArray.filter((i,indexo)=>(
+            indexo != index
+        ))
+        setTask(remainings)
+    }
   return (
-    <div>
-      task
+    <div style={{border:'1px solid black',width:"30%",marginTop:"10px",padding:"3px"}}>
+     {
+        (edit == true ? 
+        <div style={{padding:"5px"}}>
+        <input type="text" value={taskValue}
+         onChange={(e)=>setTaskValue(e.target.value)}/>
+        <button onClick={()=>{
+         const taskArray = [...task];
+         taskArray.filter(function(task,indexo){
+            if(indexo === index) {
+                task.name = taskValue;
+            }
+         })
+         setTask(taskArray);
+         setEdit(false)
+        }}>Apply changes</button>
+        </div> : (
+            <>
+            <p>Task Name : {name}</p>
+            <p>Priority : {priority}</p>
+             <button onClick={handleDelete}>delete Task</button>
+             <button onClick={()=>setEdit(true)}>Edit task</button>
+             </>
+        ))
+     }
     </div>
   )
 }
